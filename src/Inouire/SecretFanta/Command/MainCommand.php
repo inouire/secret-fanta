b87e9aa63c29bc0fb11d25720b3bb16beb6a34e6
inouire <edouard@melix.net>
2013-11-30 15:42:27 +0100

    Load list of participants from yml file

--- src/Inouire/SecretFanta/Command/MainCommand.php
+++ src/Inouire/SecretFanta/Command/MainCommand.php
@@ -7,7 +7,8 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-
+use Symfony\Component\Yaml\Yaml;
+        
 class MainCommand extends Command
 {
     protected function configure()
@@ -20,6 +21,15 @@ class MainCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        
+        // loading list of participants and display it
+        $output->writeln('Loading participants:');
+        $participants = Yaml::parse(file_get_contents('conf/chimneys.yml'));
+        foreach($participants as $name => $email){
+            $output->writeln(' - <info>'.$name.'</info> <comment>('.$email.')</comment>');
+        }
+        
+
         $output->writeln('<error>Reindeers not ready yet</error>');
     }
 }
